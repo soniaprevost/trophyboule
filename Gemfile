ec3c94bbb692719b1121cb61bd0b923daed1b16b
soniaprevost <so.prevost@gmail.com>
2015-07-08 15:02:16 +0200

    add connect with fb and gmail omniauth

--- Gemfile
+++ Gemfile
@@ -3,20 +3,22 @@ ruby "2.2.2"
 
 gem 'bootstrap-sass'
 gem 'coffee-rails', '~> 4.1.0'
+gem 'devise'
 gem 'figaro'
 gem 'font-awesome-sass'
 gem 'jbuilder', '~> 2.0'
 gem 'jquery-rails'
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
 gem 'pg'
 gem 'rails', '4.2.1'
 gem 'sass-rails', '~> 5.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'simple_form'
+gem 'slim-rails'
 gem 'turbolinks'
 gem 'uglifier', '>= 1.3.0'
 gem 'unicorn'
-gem 'slim-rails'
-gem 'devise'
 
 gem 'rails_12factor', group: :production
 gem 'puma',           group: :production
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,37 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
+  def self.find_for_facebook_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]  # Fake password for validation
+      user.name = auth.info.name
+      user.picture = auth.info.image
+      user.token = auth.credentials.token
+      user.token_expiry = Time.at(auth.credentials.expires_at)
+    end
+  end
+
+  def self.find_for_google_oauth2(auth)
+    credentials = auth.credentials
+    data = auth.info
+    user = User.where(email: data["email"]).first
+
+    unless user
+     user = User.create(
+        name: data["name"],
+        picture: data["image"],
+        email: data["email"],
+        password: Devise.friendly_token[0,20],
+        token: credentials.token,
+        token_expiry: credentials.refresh_token
+     )
+    end
+    user
+  end
 end
