74e62b6ce797c7df4ce73b53d20461ce0816eb2c
soniaprevost <so.prevost@gmail.com>
2015-07-09 19:09:35 +0200

    create, show and index games

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,46 @@
+class GamesController < ApplicationController
+  before_action :find_game, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @game.update(game_params)
+  end
+
+  def destroy
+    @game.destroy
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :startdate)
+  end
+
+  def find_game
+    @game = Game.find(params[:id])
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708110655) do
+ActiveRecord::Schema.define(version: 20150709162944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "startdate"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -40,4 +50,5 @@ ActiveRecord::Schema.define(version: 20150708110655) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "games", "users"
 end
